cad12e570c86d22e09526d706d4cb16dbe34972c
lijas <elias.borjesson@hotmail.com>
2020-04-14 19:25:36 +0200

    wip

--- src/bezier_extraction.jl
+++ src/bezier_extraction.jl
@@ -20,7 +20,7 @@ end
 
 function compute_bezier_points(Ce::BezierExtractionOperator{T}, control_points::AbstractVector{Vec{sdim,T}}, pad::Int=1) where {sdim,T}
 
-	n_points = length(control_points)#length(first(Ce))
+	n_points = length(first(Ce))#length(control_points)#
 	#@show n_points, length(control_points)
 	bezierpoints = zeros(Vec{sdim,T}, n_points)
 
--- src/splines/bezier.jl
+++ src/splines/bezier.jl
@@ -44,6 +44,7 @@ JuAFEM.faces(::IGA.BernsteinBasis{2,(2,)}) = ((1,2,3), (3,2,1))
 
 #3d Shell
 JuAFEM.faces(::BernsteinBasis{3,(2,2)}) = (1,2,3,4,5,6,7,8,9)
+JuAFEM.edges(::BernsteinBasis{3,(2,2)}) = ((1,2,3), (3,6,9), (9,8,7), (7,4,1))
 
 #3d Hexahedron
 function JuAFEM.faces(::IGA.BernsteinBasis{3,order}) where {order}
